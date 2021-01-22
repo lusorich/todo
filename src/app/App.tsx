5cbd41d92592e32232cf8c6130df188ee2ae2028
lusorich <dreamonautre@gmail.com>
2021-01-23 02:39:29 +0500

    advanced todo

---
+++ src/app/App.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import './App.css'
+import {AppBar, Button, Container, IconButton, LinearProgress, Toolbar, Typography} from '@material-ui/core'
+import {Menu} from '@material-ui/icons'
+import {TodolistsList} from '../features/TodolistsList/TodolistsList'
+import {ErrorSnackbar} from '../components/ErrorSnackbar/ErrorSnackbar'
+import {useSelector} from 'react-redux'
+import {AppRootStateType} from './store'
+import {RequestStatusType} from './app-reducer'
+
+type PropsType = {
+    demo?: boolean
+}
+
+function App({demo = false}: PropsType) {
+    const status = useSelector<AppRootStateType, RequestStatusType>((state) => state.app.status)
+    return (
+        <div className="App">
+            <ErrorSnackbar />
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" color="inherit" aria-label="menu">
+                        <Menu/>
+                    </IconButton>
+                    <Typography variant="h6">
+                        News
+                    </Typography>
+                    <Button color="inherit">Login</Button>
+                </Toolbar>
+             { status === 'loading' &&  <LinearProgress /> }
+            </AppBar>
+            <Container fixed>
+                <TodolistsList demo={demo}/>
+            </Container>
+        </div>
+    )
+}
+
+export default App
