67b2f4abc29e5ea5dcaf079df64fe81226b33149
lusorich <dreamonautre@gmail.com>
2021-04-21 19:14:22 +0500

    Updates

---
+++ precache-manifest.89eee28eec5783f7e8b5db516ebc942e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f053940dc7e88b3882fa6aa6b2a3ddba",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "d74ef052fa7812dc0dbe",
+    "url": "/todo/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "9cde3faecab2d0a70b89",
+    "url": "/todo/static/js/2.cf1fcf8b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todo/static/js/2.cf1fcf8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d74ef052fa7812dc0dbe",
+    "url": "/todo/static/js/main.5ae120d6.chunk.js"
+  },
+  {
+    "revision": "e9b3b084cc17457a6264",
+    "url": "/todo/static/js/runtime-main.c58ac565.js"
+  }
+]);
